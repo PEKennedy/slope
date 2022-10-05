4ad336a685d7a71d131e879a310b21f3ee6d43db
Peter Kennedy <Peter_E_K@outlook.com>
2022-10-05 16:18:36 -0300

    pause menu

--- app/src/main/java/ca/unb/mobiledev/slope/GameActivity.kt
+++ app/src/main/java/ca/unb/mobiledev/slope/GameActivity.kt
@@ -1,17 +1,35 @@
 package ca.unb.mobiledev.slope
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class GameActivity : AppCompatActivity() {
+
+interface CloseHandle {
+    fun close()
+}
+
+class GameActivity : AppCompatActivity(), CloseHandle {
+    private val pauseMenu = PauseMenuDialog(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
         val btnPause = findViewById<Button>(R.id.btnPause)
         btnPause.setOnClickListener {
-            finish()
+            pause()
         }
     }
+
+    private fun pause(){
+        val fragmentManager = supportFragmentManager//supportFragmentManager
+        pauseMenu.show(fragmentManager,"pause_menu")
+
+    }
+
+    override fun close() {
+        finish()
+    }
+
 }
---
+++ app/src/main/java/ca/unb/mobiledev/slope/PauseMenuDialog.kt
@@ -0,0 +1,38 @@
+package ca.unb.mobiledev.slope
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+
+
+class PauseMenuDialog(actHandle: CloseHandle) : androidx.fragment.app.DialogFragment() {
+    private val activityHandle = actHandle
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        val view = inflater.inflate(R.layout.pause_menu, container, false)
+        val btnSettings = view.findViewById<Button>(R.id.btnSettings)
+        val btnHelp = view.findViewById<Button>(R.id.btnHelp)
+        val btnQuit = view.findViewById<Button>(R.id.btnQuit)
+        val btnClose = view.findViewById<Button>(R.id.btnClose)
+
+        btnSettings.setOnClickListener {
+
+        }
+        btnHelp.setOnClickListener {
+
+        }
+        btnQuit.setOnClickListener {
+            activityHandle.close()
+        }
+        btnClose.setOnClickListener {
+            this.dismiss()
+        }
+
+        return view
+    }
+}
